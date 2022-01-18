0ee15c106a7db226fae9c02c16dca9555ee1f083
Juan Carlos García Rodríguez <jcgr1968@gmail.com>
2022-01-18 15:36:48 +0100

    Inicio

---
+++ core/erp/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from datetime import datetime
+from core.erp.choices import gender_choices
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['id']
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Nombre')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product', null=True, blank=True, verbose_name='Imágen')
+    price = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Precio')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ['id']
+
+class Customer(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre')
+    surname = models.CharField(max_length=150, verbose_name='Apellidos')
+    dni = models.CharField(max_length=9, unique=True, verbose_name='DNI')
+    birthday = models.DateField(verbose_name='Fecha de nacimiento')
+    address = models.CharField(max_length=150, null=True, blank=True, verbose_name='Dirección')
+    sex = models.CharField(max_length=10, choices=gender_choices)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        ordering = ['id']
+
+class Sale(models.Model):
+    cli = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date = models.DateField(default=datetime.now())
+    subtotal = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    iva = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    total = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.cli.name
+
+    class Meta:
+        verbose_name = 'Venta'
+        verbose_name_plural = 'Ventas'
+        ordering = ['id']
+
+class DetSale(models.Model):
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0, verbose_name='Cantidad')
+    price = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Precio')
+    subtotal = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Subtotal')
+
+    def __str__(self):
+        return self.product.name
+
+    class Meta:
+        verbose_name = 'Detalle de Venta'
+        verbose_name_plural = 'Detalles de Venta'
+        ordering = ['id']
